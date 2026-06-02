5da5d28ab0afdcbe672508da802250978fe723b2
robertsouzza <rcsouzza@gmail.com>
2026-06-02 16:57:05 -0400

    feat: setup project environment with docker-compose and spring boot backend core

---
+++ cerebro-backend/src/main/java/com/omnicore/cerebro_backend/CerebroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.omnicore.cerebro_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CerebroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CerebroBackendApplication.class, args);
+	}
+
+}
---
+++ cerebro-backend/src/test/java/com/omnicore/cerebro_backend/CerebroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omnicore.cerebro_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CerebroBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
